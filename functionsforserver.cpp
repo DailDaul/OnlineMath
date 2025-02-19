3af453ec050363b8de0d3881750d1504371db394
Kyannittee <Ann31032006@gmail.com>
2025-02-19 20:41:00 +0300

    Update functionsforserver.cpp

--- functionsforserver.cpp
+++ functionsforserver.cpp
@@ -1,35 +1,42 @@
 #include "functionsforserver.h"
+#include <QDebug>
+#include <QCoreApplication>
+#include<QString>
 //#include "dataBase.h"
-QByteArray parsing (QString data_from_client){
-    /* data_from_client = NameOfFunc&Param1&Param2&Param3
-     * Цель парсинга:
-     * return NameOfFunc(Param1, Param2, Param3)
-     */
-    QStringList data_from_client_list = data_from_client.split(QLatin1Char('&'));//использует & в качестве разделителя и сохраняет данные в data_from_client_list
-    // data_from_client_list = "NameOfFunc" "Param1" "Param2" "Param3"
-    QString nameOfFunc = data_from_client_list.front();// получает первое значение из списка и сохраняет его
+QByteArray parsing(QString data_from_client) {
+    // Проверка на наличие символа '&'
+    if (!data_from_client.contains('&')) {
+        qDebug() << "Ошибка: неверный формат данных. Введенная строка:" << data_from_client;
+        return "Используйте reg/auth для регистрации или авторизации\n";
+    }
+
+    // Если символ '&' найден, продолжаем парсинг
+    QStringList data_from_client_list = data_from_client.split(QLatin1Char('&'));
+    QString nameOfFunc = data_from_client_list.front();
     data_from_client_list.pop_front();
-    if (nameOfFunc == "auth"){
-        if (data_from_client_list.size() < 2){
+
+    if (nameOfFunc == "auth") {
+        if (data_from_client_list.size() < 2) {
             return "Error: not enough parameters for auth\n";
         }
-        return auth(data_from_client_list.at(0),data_from_client_list.at(1));
-    }
-    else if (nameOfFunc == "reg"){
-        if (data_from_client_list.size() < 3){
+        return auth(data_from_client_list.at(0), data_from_client_list.at(1));
+    } else if (nameOfFunc == "reg") {
+        if (data_from_client_list.size() < 3) {
             return "Error: not enough parameters for reg\n";
         }
-        return reg(data_from_client_list.at(0),data_from_client_list.at(1), data_from_client_list.at(2));
-    }
-    else
+        return reg(data_from_client_list.at(0), data_from_client_list.at(1), data_from_client_list.at(2));
+    } else {
         return "Error: unknown function\n";
+    }
 }
-
 QByteArray auth(QString log, QString pass){
     //DataBase::sendQuerry("xcbvm");
-    return "auth\n";
+    QString result = QString("auth&%1&%2").arg(log).arg(pass);
+    return result.toUtf8() + "\n";
 }
 
 QByteArray reg(QString log, QString pass, QString mail){
-    return "reg\n";
+    QString result = QString("reg&%1&%2&%3").arg(log).arg(pass).arg(mail);
+    return result.toUtf8() + "\n";
 }
+
