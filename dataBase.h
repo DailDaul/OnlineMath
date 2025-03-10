ecf869dd008ffd53ec01dd0b83c0357a1311caaa
Kyannittee <Ann31032006@gmail.com>
2025-03-10 16:37:13 +0300

    Update dataBase.h

--- dataBase.h
+++ dataBase.h
@@ -1,23 +1,29 @@
-/*#ifndef DATABASE_H
+#ifndef DATABASE_H
 #define DATABASE_H
 
-#include <QSqlDatabase>
-#include <QSqlQuery>
-#include <QSqlError>
-#include <QDebug>
+#include <QSqlDatabase>//класс для работы с базами данных
+#include <QSqlQuery>//класс для выполнения SQL-запросов
+#include <QSqlError>//класс для обработки ошибок, связанных с базой данных
+#include <QDebug>//класс для отладки, позволяющий выводить сообщения в консоль
 
 class DataBase {
 public:
-    DataBase(const QString &host, const QString &dbName, const QString &user, const QString &password);
+    DataBase(const QString &host, const QString &dbName, const QString &user, const QString &password);//Конструктор, который принимает параметры для подключения к базе данных: хост, имя базы данных, имя пользователя и пароль
     ~DataBase();
-
-    bool open();
-    void close();
-    bool createUser (const QString &login, const QString &password, const QString &email);
-    void listUsers();
+    bool createTables();//Метод для создания таблиц
+    bool open();//Метод для открытия соединения с базой данных
+    bool isOpen() const;
+    void close();//Метод для закрытия соединения с базой данных
+    int createUser(const QString &login, const QString &password, const QString &email, QString& errorMessage);//Метод для создания нового пользователя в базе данных
+    bool authenticateUser(const QString &login, const QString &password);//Метод для аутентификации пользователя
+    bool saveCalculation(int userId, const QString &operation, const QString &result);//Метод для сохранения результатов вычислений в базе данных
+    QList<QMap<QString, QVariant>> getHistory(int userId, int limit = 30);//Метод для получения истории операций пользователя. Возвращает список, содержащий карты, где ключи — это строки, а значения — это объекты QVariant
+    void listUsers();//Метод для вывода списка пользователей
+    void dropUserTable();//Метод для удаления таблицы пользователей из базы данных
+    int getUserId(const QString &login);
 
 private:
-    QSqlDatabase db;
+    QSqlDatabase db;//Объект базы данных
 };
 
-#endif // DATABASE_H*/
+#endif // DATABASE_H
