3a7fabce093a4b8a1ae248b76704b382c289bc8b
Kyannittee <Ann31032006@gmail.com>
2025-03-10 16:46:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,31 +9,11 @@
 #include "dataBase.h"
 #include "mytcpserver.h"
 
+
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     MyTcpServer myserv;
-
-    /*DataBase db("localhost", "TestDB", "your_username", "your_password");
-
-    if (!db.open()) {
-        return -1; // Завершить программу при ошибке
-    }
-
-    // Создание пользователей
-    db.createUser ("admin", "123", "admin@example.com");
-    db.createUser ("user", "123", "user@example.com");
-
-    // Вывод списка пользователей
-    db.listUsers();
-
-    // Пример авторизации
-    if (db.authenticateUser ("admin", "123")) {
-        qDebug() << "Авторизация успешна!";
-    } else {
-        qDebug() << "Ошибка авторизации!";
-    }
-
-    db.close();*/
     return a.exec();
 }
