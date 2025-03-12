2d603b94bd3c4d5b553ce8ec24cbf3c327123798
Kyannittee <Ann31032006@gmail.com>
2025-03-12 16:52:11 +0300

    Update functionsforserver.h

--- functionsforserver.h
+++ functionsforserver.h
@@ -6,4 +6,6 @@
 QByteArray parsing(QString data_from_client);
 QByteArray auth(QString log, QString pass, QString mail = "");
 QByteArray reg(QString log, QString pass, QString mail);
+QByteArray saveCalculation(int userId, const QString& operation);
+QByteArray getHistory(int userId, int limit);
 #endif // FUNCTIONSFORSERVER_H
