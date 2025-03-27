d2e91223bc1b527f6914c3ae7ed025b312f6a88e
DailDaul <taisiyakn@gmail.com>
2025-03-27 17:26:28 +0300

    Update mytcpserver.cpp
    добавила функцию для работы нескольких клиентов и чуть переписала для корректной работы функции

--- mytcpserver.cpp
+++ mytcpserver.cpp
@@ -6,18 +6,18 @@
 
 MyTcpServer::~MyTcpServer()
 {
-
-    mTcpServer->close();
+    mTсpServer->close();
     //server_status=0;
 }
 
 MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent){
-    mTcpServer = new QTcpServer(this);//выделяем память под сервер
+    mTсpServer = new QTcpServer(this);
+    mSockets = QList<QTcpSocket*>();
 
-    connect(mTcpServer, &QTcpServer::newConnection,//связываем сигнал который издает сервер с тем действием которое он же должен совершить
+    connect(mTсpServer, &QTcpServer::newConnection,//связываем сигнал который издает сервер с тем действием которое он же должен совершить
             this, &MyTcpServer::slotNewConnection);//источник сигнала, имя сигнала, получатель сигнала, что должен исполнить получатель
 
-    if(!mTcpServer->listen(QHostAddress::Any, 33333)){//с любого айпи на порт 33333
+    if(!mTсpServer->listen(QHostAddress::Any, 33333)){//с любого айпи на порт 33333
         qDebug() << "server is not started";
     } else {
         //server_status=1;
@@ -26,23 +26,34 @@ MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent){
 }
 
 void MyTcpServer::slotNewConnection(){
-    mTcpSocket = mTcpServer->nextPendingConnection();
-    if (!mTcpSocket->isOpen()) {
+    QTcpSocket *socket =  mTсpServer->nextPendingConnection(); //Получаем сокет нового подключения
+    if (!socket->isOpen()) {
         qDebug() << "Сокет не открыт";
+        socket->deleteLater(); //Удаляем сокет, если он не открылся
         return;
     }
+
     //if (!mTcpSocket->write("Hello!!! I am calculator!\r\n")) {
     //   qDebug() << "Ошибка записи в сокет";
     //    return;
     //}
-    connect(mTcpSocket, &QTcpSocket::readyRead, this, &MyTcpServer::slotServerRead);//если готов к чтению
-    connect(mTcpSocket, &QTcpSocket::disconnected, this, &MyTcpServer::slotClientDisconnected);//если клиент отключился
+    connect(socket, &QTcpSocket::readyRead, this, &MyTcpServer::slotServerRead);//если готов к чтению, используем сокет
+    connect(socket, &QTcpSocket::disconnected, this, &MyTcpServer::slotClientDisconnected);//если клиент отключился, используем сокет
+
+    mSockets.append(socket); // Добавляем сокет в список активных сокетов
+    qDebug() << "Новое подключение. Всего клиентов:" << mSockets.size();
 }
 
 void MyTcpServer::slotServerRead() {
+    QTcpSocket *socket = qobject_cast<QTcpSocket*>(sender()); // Определяем сокет, который вызвал сигнал
+
+    if (!socket) {
+        qDebug() << "Ошибка: не удалось определить сокет, вызвавший сигнал";
+        return;
+    }
     QString res = "";
-    while (mTcpSocket->bytesAvailable() > 0) {
-        QByteArray array = mTcpSocket->readAll();
+    while (socket->bytesAvailable() > 0) {
+        QByteArray array = socket->readAll();
         if (array.isEmpty()) {
             qDebug() << "Пустой массив";
             return;
@@ -54,31 +65,37 @@ void MyTcpServer::slotServerRead() {
 
 
     QByteArray response = parsing(res); // Получаем ответ от функции parsing
-    if (!mTcpSocket->write(response)) { // Записываем response в сокет
+    if (!socket->write(response)) { // Записываем response в сокет
         qDebug() << "Ошибка записи в сокет";
         return;
     }
 }
 
 void MyTcpServer::slotClientDisconnected(){
-    // 1. Отключаем все сигналы и слоты
-    disconnect(mTcpSocket, nullptr, this, nullptr);
+    QTcpSocket *socket = qobject_cast<QTcpSocket*>(sender());
 
-    // 2. Убеждаемся, что все данные отправлены (пример)
-    if (mTcpSocket->bytesToWrite() > 0) {
-        mTcpSocket->flush(); // Пытаемся отправить оставшиеся данные
-        mTcpSocket->waitForBytesWritten(1000); // Ждем 1 секунду
+    if (!socket) {
+        qDebug() << "Ошибка: не удалось определить сокет, вызвавший сигнал об отключении";
+        return;
+    }
+
+    // 1. Отключаем все сигналы и слоты для этого сокета
+    disconnect(socket, nullptr, this, nullptr);
+
+    // 2. Убеждаемся, что все данные отправлены
+    if (socket->bytesToWrite() > 0) {
+        socket->flush(); // Пытаемся отправить оставшиеся данные
+        socket->waitForBytesWritten(1000); // Ждем 1 секунду
     }
 
     // 3. Закрываем сокет
-    mTcpSocket->disconnectFromHost();
-
-    // 4. Проверяем состояние сокета перед вызовом waitForDisconnected()
-    if (mTcpSocket->state() == QAbstractSocket::ConnectedState) {
-        mTcpSocket->waitForDisconnected(1000); // Ждем отключения
+    socket->disconnectFromHost();
+    if (socket->state() == QAbstractSocket::ConnectedState) {
+        socket->waitForDisconnected(1000); // Ждем отключения
     }
 
-    // 5. Удаляем сокет безопасно
-    mTcpSocket->deleteLater();
-    mTcpSocket = nullptr;
+    // 4. Удаляем сокет из списка и безопасно удаляем его
+    mSockets.removeOne(socket);
+    socket->deleteLater();
+    qDebug() << "Клиент отключился. Осталось клиентов:" << mSockets.size();
 }
